805354fba5a9faf9db33491058233bf8d884fa6d
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2024-06-27 21:40:54 +0530

    Added

---
+++ CarRacingGame-CGMiniProject.cpp
@@ -0,0 +1,264 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<GL/glut.h>
+
+bool gameStarted = false;
+float car_x,road_y,speed=10;
+void draw_road(int x, int y) {
+	glColor3f(0.2, 0.2, 0.2);
+	//printf("Hello world");
+	glBegin(GL_POLYGON);
+	glVertex2f(-100.0+x, -300.0+y);
+	glVertex2f(-100.0+x, 300.0+y);
+	glVertex2f(100.0+x, 300.0+y);
+	glVertex2f(100.0+x, -300.0+y);
+	glEnd();
+
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	for (int i = -280; i < 280; i += 40) {
+		glVertex2f(0.0+x, i+y);
+		glVertex2f(0.0+x, i + 20+y);
+	}
+	glEnd();
+}
+void draw_car(int x, int y) {
+
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);
+	glVertex2f(-30.0+x, -50.0+y);
+	glVertex2f(-30.0+x, 50.0+y);
+	glVertex2f(30.0+x, 50.0+y);
+	glVertex2f(30.0+x, -50.0+y);
+	glEnd();
+
+	//Front window(grey color)
+	glColor3f(0.5, 0.5, 0.5);
+	glBegin(GL_POLYGON);
+	glVertex2f(-20.0 + x, 30.0 + y);
+	glVertex2f(-10.0 + x, 10.0 + y);
+	glVertex2f(10.0 + x, 10.0 + y);
+	glVertex2f(20.0 + x, 30.0 + y);
+	glEnd();
+
+	//Rear Window(grey color)
+	glBegin(GL_POLYGON);
+	glVertex2f(-20.0 + x, -40.0 + y);
+	glVertex2f(-10.0 + x, -20.0 + y);
+	glVertex2f(10.0 + x, -20.0 + y);
+	glVertex2f(20.0 + x, -40.0 + y);
+	glEnd();
+
+	// Side mirrors
+	glColor3f(0.5, 0.5, 0.5);
+	glBegin(GL_POLYGON); // Left mirror
+	glVertex2f(-32.0 + x, 7.0 + y);
+	glVertex2f(-42.0 + x, 14.0 + y);
+	glVertex2f(-32.0 + x, 27.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON); // Right mirror
+	glVertex2f(31.0 + x, 7.0 + y);
+	glVertex2f(41.0 + x, 14.0 + y);
+	glVertex2f(31.0 + x, 27.0 + y);
+	glEnd();
+
+	//Wheels
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);//Left rear wheel
+	glVertex2f(-40.0 + x, -37.0 + y);
+	glVertex2f(-30.0 + x, -37.0 + y);
+	glVertex2f(-30.0 + x, -23.0 + y);
+	glVertex2f(-40.0 + x, -23.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON);//Right rear wheel
+	glVertex2f(40.0 + x, -37.0 + y);
+	glVertex2f(30.0 + x, -37.0 + y);
+	glVertex2f(30.0 + x, -23.0 + y);
+	glVertex2f(40.0 + x, -23.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON);//Left front wheel
+	glVertex2f(-40.0 + x, 37.0 + y);
+	glVertex2f(-30.0 + x, 37.0 + y);
+	glVertex2f(-30.0 + x, 23.0 + y);
+	glVertex2f(-40.0 + x, 23.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON);//Right front wheel
+	glVertex2f(40.0 + x, 37.0 + y);
+	glVertex2f(30.0 + x, 37.0 + y);
+	glVertex2f(30.0 + x, 23.0 + y);
+	glVertex2f(40.0 + x, 23.0 + y);
+	glEnd();
+
+	// Additional details (yellow polygons)
+	glColor3f(0.2, 0.2, 0.2);
+	glBegin(GL_POLYGON); // Front left detail
+	glVertex2f(-30.0 + x, 25.0 + y);
+	glVertex2f(-30.0 + x, 50.0 + y);
+	glVertex2f(-20.0 + x, 50.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON); // Front right detail
+	glVertex2f(30.0 + x, 25.0 + y);
+	glVertex2f(30.0 + x, 50.0 + y);
+	glVertex2f(20.0 + x, 50.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON); // Rear left detail
+	glVertex2f(-30.0 + x, 50.0 + y);
+	glVertex2f(-20.0 + x, 40.0 + y);
+	glVertex2f(-30.0 + x, 40.0 + y);
+	glEnd();
+
+	glBegin(GL_POLYGON); // Rear right detail
+	glVertex2f(30.0 + x, 50.0 + y);
+	glVertex2f(20.0 + x, 40.0 + y);
+	glVertex2f(30.0 + x, 40.0 + y);
+	glEnd();
+}
+void draw_obstacle(int x, int y);
+
+void init() {
+	//glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-400.0, 400.0, -300.0, 300.0); 
+	glMatrixMode(GL_MODELVIEW);
+}
+void reshape(int w, int h) {
+	glViewport(0, 0, w, h); 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity(); 
+
+	// Calculate aspect ratio of the new window
+	float aspect = (float)w / (float)h;
+
+	
+	if (w >= h) {
+		gluOrtho2D(-400.0 * aspect, 400.0 * aspect, -300.0, 300.0);
+	}
+	else {
+		gluOrtho2D(-400.0, 400.0, -300.0 / aspect, 300.0 / aspect);
+	}
+
+	glMatrixMode(GL_MODELVIEW); 
+}
+
+void drawString(float cx, float cy, const char* str) {
+	glRasterPos2f(cx, cy);
+	for (int i = 0; i < strlen(str); i++) {
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, str[i]);
+	}
+}
+
+void frontPage() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+
+	drawString(-200.0, 150.0, "Name: Harsh RG");
+	drawString(-200.0, 100.0, "USN: 1RN21CS062");
+	drawString(-200.0, 50.0, "Section: 6th sem CSE 'A' section");
+	drawString(-240.0, 0.0, "Guide: Dr Sudhamani MJ, Associate Professor, CSE Dept");
+
+	// Instructions of the game
+	glColor3f(1.0, 1.0, 0.0);
+	drawString(-100.0, -75.0, "Instructions");
+	drawString(-200.0, -125.0, "1. Press 's' OR 'S' key to Start the game.");
+	drawString(-200.0, -150.0, "2. Press 'L' for car to move left.");
+	drawString(-200.0, -175.0, "3. Press 'R' for car to move right.");
+	drawString(-200.0, -200.0, "4. Press 'U' to speed up car.");
+	drawString(-200.0, -225.0, "5. Start Game by 's' Or 'S' key");
+	glutSwapBuffers();
+}
+void startGame() {
+	gameStarted = true;
+	printf("Hello world\n");
+	glutPostRedisplay();
+}
+void display() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	if (!gameStarted) {
+		frontPage();
+	}
+	else {
+		glClearColor(1.0, 1.0, 1.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		draw_road(0,road_y);
+		draw_road(0,road_y-600);
+		draw_road(0,road_y+600);
+		draw_car(car_x, -150);
+		glutSwapBuffers();
+	}
+}
+void keyboard(unsigned char key, int x, int y) {
+	if (key == 's' || key == 'S') {
+		startGame();
+	}
+	else if (key == 'w' || key == 'W') {
+		road_y += speed;
+		if (road_y > 600) {
+			road_y -= 600;
+		}
+	}
+	else if (key == 'u' || key == 'U') {
+		road_y -= speed;
+		if (road_y < 0) {
+			road_y += 600;
+		}
+	}
+	else if (key == 'l' || key == 'L') {
+		car_x-=3;
+		if (car_x < -70) {
+			car_x = -70;
+		}
+	}
+	else if (key == 'r' || key == 'R') {
+		car_x+=3;
+		if (car_x > 70) {
+			car_x = 70;
+		}
+	}
+	else if (key == '-') {
+		speed--;
+		if (speed < 0) {
+			speed = 0;
+		}
+	}
+	else if (key == '=') {
+		speed++;
+		if (speed > 100) {
+			speed = 100;
+		}
+	}
+	glutPostRedisplay();
+}
+void idle(int t) {
+	road_y -= speed/10;
+	//printf("%f,%f\n", speed,road_y);
+	if (road_y <0) {
+		road_y += 600;
+	}
+	glutTimerFunc(50, idle,0);
+	glutPostRedisplay();
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("2D Car Racing Game");
+
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutTimerFunc(1000,idle,0);
+	glutMainLoop();
+	return 0;
+}
